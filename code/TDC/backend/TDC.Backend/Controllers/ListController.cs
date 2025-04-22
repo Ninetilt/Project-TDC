bb864ddee1488b991ba8485fd87d7b4334e8e840
Hannah Moog <hannah.moog@generic.de>
2025-04-22 11:57:06 +0200

    changed program

---
+++ code/TDC/backend/TDC.Backend/Controllers/ListController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDC.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class ListController : ControllerBase
+    {
+        [HttpPut("createList")]
+        public async Task<long> CreateToDoList()
+        {
+            return 0;
+        }
+
+        [HttpPost("updateList")]
+        public async Task UpdateToDoList()
+        {
+            return;
+        }
+
+        [HttpGet("test")]
+        public async Task Test()
+        {
+        }
+    }
+}
