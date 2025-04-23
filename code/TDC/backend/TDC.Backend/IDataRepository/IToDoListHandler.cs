c3135268fbb48f983da581177b55f7059d24c8a2
Hannah Moog <hannah.moog@generic.de>
2025-04-23 10:46:09 +0200

    added interfaces for user and list

---
+++ code/TDC/backend/TDC.Backend/IDataRepository/IToDoListHandler.cs
@@ -0,0 +1,12 @@
+﻿using TDC.Backend.IDomain.Models;
+
+namespace TDC.Backend.IDataRepository
+{
+    public interface IToDoListHandler
+    {
+        public long CreateList(ToDoListDto newList);
+        public void UpdateListTitle(long listId, string newTitle);
+        public void DeleteList(long listId, long userId);
+        public void FinishList(long listId, long userId);
+    }
+}
