fbdafa109a98e43c24cc005566c626dd82b6381b
Hannah Moog <hannah.moog@generic.de>
2025-04-23 10:17:59 +0200

    added all relevant list management endpoints and models on domain side

--- code/TDC/backend/TDC.Backend/Controllers/ListController.cs
+++ code/TDC/backend/TDC.Backend/Controllers/ListController.cs
@@ -1,28 +1,93 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using TDC.Backend.IDomain.Models;
 
 namespace TDC.Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+
     public class ListController : ControllerBase
     {
-        [HttpPut("createList")]
-        public async Task<long> CreateToDoList()
+        #region To-Do-List
+        [HttpPut("createList/{userId}")]
+        public async Task<long> CreateToDoList([FromBody] ToDoListDto listDto)
         {
-            return 0;
+            throw new NotImplementedException();
         }
 
-        [HttpPost("updateList")]
-        public async Task UpdateToDoList()
+        public class StringHelper
         {
-            return;
+            public string Text { get; set; }
         }
 
-        [HttpGet("test")]
-        public async Task Test()
+        [HttpPost("updateListTitle/{listId}/{userId}")]
+        public async Task UpdateToDoList([FromRoute] long listId, [FromRoute] long userId, [FromBody] StringHelper newTitle)
         {
+            throw new NotImplementedException();
         }
+
+        [HttpDelete("deleteList/{listId}/{userId}")]
+        public async Task DeleteToDoList([FromRoute] long listId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("finishList/{listId}/{userId}")]
+        public async Task FinishToDoList([FromRoute] long listId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("addUserToList/{listId}/{userId}")]
+        public async Task AddUserToList([FromRoute] long listId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("getListsForUser/{userId}")]
+        public async Task<ToDoListDto> GetListsForUser([FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("getListById/{listId}/{userId}")]
+        public async Task<ToDoListDto> GetListById([FromRoute] long listId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region List-Items
+
+        [HttpPut("addItemToList/{listId}")]
+        public async Task AddItemToList([FromRoute] long listId, [FromBody] StringHelper itemText, [FromBody] long itemEffort)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("updateItemText/{itemId}")]
+        public async Task UpdateItemText([FromRoute] long itemId, [FromBody] StringHelper newText)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("updateItemEffort/{itemId}")]
+        public async Task UpdateItemEffort([FromRoute] long itemId, [FromBody] long newEffort)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("setItemStatusDone/{itemId}/{userId}")]
+        public async Task SetItemStatusDone([FromRoute] long itemId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("setItemStatusNotDone/{itemId}/{userId}")]
+        public async Task SetItemStatusNotDone([FromRoute] long itemId, [FromRoute] long userId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
---
+++ code/TDC/backend/TDC.Backend/IDomain/Models/ToDoListDTO.cs
@@ -0,0 +1,3 @@
+﻿namespace TDC.Backend.IDomain.Models;
+
+public record ToDoListDto(long listId, string name, List<ToDoListItemDto> items, List<long> members, bool isCollaborative, bool isFinished);
