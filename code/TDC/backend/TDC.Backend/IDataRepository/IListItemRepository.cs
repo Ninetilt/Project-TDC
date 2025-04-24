ad8f941e1bbde6c6c32f4f05620173180601a7e1
Hannah Moog <hannah.moog@generic.de>
2025-04-24 12:54:41 +0200

    removed userId from list object and fixed list repos

--- code/TDC/backend/TDC.Backend/IDataRepository/IListItemRepository.cs
+++ code/TDC/backend/TDC.Backend/IDataRepository/IListItemRepository.cs
@@ -9,7 +9,6 @@ namespace TDC.Backend.IDataRepository
         public void RemoveItemFromList(long itemId);
         public void UpdateItemDescription(long itemId, string description);
         public void UpdateItemEffort(long itemId, uint effort);
-
         public void SetItemStatus(long itemId, long userId, bool status);
         public bool GetItemStatus(long itemId, long userId);
     }
