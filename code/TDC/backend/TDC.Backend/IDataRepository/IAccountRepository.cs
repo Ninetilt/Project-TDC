0b5507f1b6a58e01c50d9f0eea467d3743f0e38e
Hannah Moog <hannah.moog@generic.de>
2025-04-24 14:00:34 +0200

    added account repos interface

---
+++ code/TDC/backend/TDC.Backend/IDataRepository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using TDC.Backend.IDataRepository.Models;
+
+namespace TDC.Backend.IDataRepository
+{
+    public interface IAccountRepository
+    {
+        public void AddAccount(AccountDbo account);
+        public void DeleteAccount(string username);
+        public void UpdateAccount(AccountDbo account);
+        public AccountDbo GetAccountByUsername(string username);
+        public AccountDbo GetAccountByEmail(string email);
+    }
+}
