c9a17939b9185f029d33084547f7ae0fd680a3f1
Hannah Moog <hannah.moog@generic.de>
2025-04-24 11:51:09 +0200

    added list member repos

---
+++ code/TDC/backend/TDC.Backend/IDataRepository/IListMemberRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TDC.Backend.IDataRepository
+{
+    public interface IListMemberRepository
+    {
+        public List<long> GetMembersForList(long listId);
+        public void AddListMember(long listId, long userId);
+        public void RemoveListMember(long listId, long userId);
+    }
+}
