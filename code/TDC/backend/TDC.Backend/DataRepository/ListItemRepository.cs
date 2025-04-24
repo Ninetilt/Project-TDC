aaceedac585d687c6cbb2d42b65671900febd2d5
Hannah Moog <hannah.moog@generic.de>
2025-04-24 10:15:42 +0200

    created interface for item repository

---
+++ code/TDC/backend/TDC.Backend/DataRepository/ListItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using TDC.Backend.IDataRepository;
+using TDC.Backend.IDataRepository.Models;
+
+namespace TDC.Backend.DataRepository
+{
+    public class ListItemRepository: IListItemRepository
+    {
+        public void AddItemToList(long listId, ToDoListItemDbo item)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveItemFromList(long itemId)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateItemDescription(long itemId, string description)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateItemEffort(long itemId, uint effort)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetItemStatus(long itemId, long userId, bool status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
