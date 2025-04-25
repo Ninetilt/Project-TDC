3f8233143187ecda21af20515ea4c71ab870af07
Hannah Moog <hannah.moog@generic.de>
2025-04-25 14:00:23 +0200

    added classes for handler interfaces

---
+++ code/TDC/backend/TDC.Backend/Domain/AccountHandler.cs
@@ -0,0 +1,78 @@
+﻿using TDC.Backend.IDomain;
+using TDC.Backend.IDomain.Models;
+
+namespace TDC.Backend.Domain
+{
+    public class AccountHandler : IAccountHandler
+    {
+        public bool AcceptFriendRequest(string username, string requestName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelFriendRequest(string sender, string receiver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DenyFriendRequest(string username, string requestName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountLoadingDto GetAccountData(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFriendsForUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetRequestsForUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoginWithMail(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoginWithUsername(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterUser(AccountSavingDto accountData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendFriendRequest(string sender, string receiver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEmail(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePassword(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUserDescription(string username, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUsername(string oldUsername, string newUsername)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ code/TDC/backend/TDC.Backend/Domain/ToDoListHandler.cs
@@ -0,0 +1,68 @@
+﻿using TDC.Backend.IDomain;
+using TDC.Backend.IDomain.Models;
+
+namespace TDC.Backend.Domain
+{
+    public class ToDoListHandler : IToDoListHandler
+    {
+        public long AddItemToList(long listId, string itemDescription, uint itemEffort)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUserToList(long listId, string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long CreateList(ToDoListDto newList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(long itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteList(long listId, long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinishList(long listId, long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoListDto> GetListsForUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUserFromList(long listId, string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetItemStatus(long itemId, string updateForUser, bool isDone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItemDescription(long itemId, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItemEffort(long itemId, uint effort)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateListTitle(long listId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
