d758f876add928187228dfb362b13458e56a4558
Hannah Moog <hannah.moog@generic.de>
2025-05-21 18:09:56 +0200

    updated sql migration foreign key constraint

---
+++ code/TDC/TDC-Backend.Test/RepositoryTests/OpenRewardsRepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using TDC.Backend.DataRepository.Test;
+using TDC.Backend.DataRepository;
+
+namespace TDC.Backend.Test.RepositoryTests
+{
+    public class OpenRewardsRepositoryTests
+    {
+        private OpenRewardsRepository _target;
+        private ListRepository _listRepository;
+        private AccountRepository _accountRepository;
+        private ListRewardingRepository _listRewardingRepository;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var connectionFactory = TestStartUp.GetConnectionFactory();
+            this._target = new OpenRewardsRepository(connectionFactory);
+            _listRepository = new ListRepository(connectionFactory);
+            _accountRepository = new AccountRepository(connectionFactory);
+            _listRewardingRepository = new ListRewardingRepository(connectionFactory);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this._target.CleanTable();
+            _listRepository.CleanTable();
+            _accountRepository.CleanTable();
+            _listRewardingRepository.CleanTable();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this._target.CleanTable();
+            _listRepository.CleanTable();
+            _accountRepository.CleanTable();
+            _listRewardingRepository.CleanTable();
+        }
+    }
+}
--- code/TDC/backend/TDC.Backend/Database/migrations/V1_4_0__AddListRewardingTables.sql
+++ code/TDC/backend/TDC.Backend/Database/migrations/V1_4_0__AddListRewardingTables.sql
@@ -10,7 +10,7 @@ CREATE TABLE[dbo].[OpenRewards] (
     [ListId] [bigint] NOT NULL,
     [Username] [nvarchar](16) NOT NULL,
     CONSTRAINT[PK_OpenRewards] PRIMARY KEY CLUSTERED([ListId] ASC, [Username]),
-    CONSTRAINT[FK_ListId_OpenRewards] FOREIGN KEY([ListId]) REFERENCES[dbo].[ToDoList]([Id]) ON DELETE CASCADE,
+    CONSTRAINT[FK_ListId_OpenRewards] FOREIGN KEY([ListId]) REFERENCES[dbo].[Rewarding]([ListId]) ON DELETE CASCADE,
     CONSTRAINT[FK_Username_OpenRewards] FOREIGN KEY([Username]) REFERENCES[dbo].[Account]([Username]) ON DELETE CASCADE
     );
 GO
